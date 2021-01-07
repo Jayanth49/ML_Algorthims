71f27d136783d2b14f39120c3d4d01656db660fa
Jayy <47641025+Jayanth49@users.noreply.github.com>
2021-01-07 18:11:49 +0530

    Update custom.py

--- Deep_Learning/Convnets/res/custom.py
+++ Deep_Learning/Convnets/res/custom.py
@@ -65,15 +65,16 @@ class Accumulator:
     def __getitem__(self, idx):
         return self.data[idx]
     
-def get_dataloader_workers():
-    """Use 4 processes to read the data."""
-    return 4
     
 def get_fashion_mnist_labels(labels):
     """Return text labels for the Fashion-MNIST dataset."""
     text_labels = ['t-shirt', 'trouser', 'pullover', 'dress', 'coat',
                    'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
     return [text_labels[int(i)] for i in labels]
+
+def get_dataloader_workers():
+    """Use 4 processes to read the data."""
+    return 4
 
 
 def load_data_fashion_mnist(batch_size, resize=None):
@@ -92,13 +93,47 @@ def load_data_fashion_mnist(batch_size, resize=None):
                             num_workers=get_dataloader_workers()))
 
 
+def accuracy(y_hat, y):
+    """Compute the number of correct predictions."""
+    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
+        y_hat = torch.argmax(y_hat, axis=1)        
+    cmp = torch.astype(y_hat, y.dtype) == y
+    return float(torch.reduce_sum(torch.astype(cmp, y.dtype)))
+
+def evaluate_accuracy(net, data_iter):
+    """Compute the accuracy for a model on a dataset."""
+    if isinstance(net, torch.nn.Module):
+        net.eval()  # Set the model to evaluation mode
+    metric = Accumulator(2)  # No. of correct predictions, no. of predictions
+    for X, y in data_iter:
+        metric.add(accuracy(net(X), y), torch.size(y))
+    return metric[0] / metric[1]
+
+def evaluate_accuracy_gpu(net, data_iter, device=None):
+    """Compute the accuracy for a model on a dataset using a GPU."""
+    if isinstance(net, torch.nn.Module):
+        net.eval()  # Set the model to evaluation mode
+        if not device:
+            device = next(iter(net.parameters())).device
+    # No. of correct predictions, no. of predictions
+    metric = Accumulator(2)
+    for X, y in data_iter:
+        if isinstance(X, list):
+            # Required for BERT Fine-tuning (to be covered later)
+            X = [x.to(device) for x in X]
+        else:
+            X = X.to(device)
+        y = y.to(device)
+        metric.add(accuracy(net(X), y), torch.size(y))
+    return metric[0] / metric[1]
 
     
-def train_ch6(net, train_iter, test_iter, num_epochs, lr):
+def training_conv(net, train_iter, test_iter, num_epochs, lr):
     """Train a model with a GPU (defined in Chapter 6)."""
     def init_weights(m):
         if type(m) == nn.Linear or type(m) == nn.Conv2d:
             nn.init.xavier_uniform_(m.weight)
+            
     net.apply(init_weights)
     print('training on', device)
     net.to(device)
@@ -117,12 +152,9 @@ def train_ch6(net, train_iter, test_iter, num_epochs, lr):
             l.backward()
             optimizer.step()
             with torch.no_grad():
-                metric.add(l * X.shape[0], d2l.accuracy(y_hat, y), X.shape[0])
+                metric.add(l * X.shape[0], accuracy(y_hat, y), X.shape[0])
             train_l = metric[0] / metric[2]
             train_acc = metric[1] / metric[2]
         test_acc = evaluate_accuracy_gpu(net, test_iter)
-        animator.add(epoch + 1, (None, None, test_acc))
     print(f'loss {train_l:.3f}, train acc {train_acc:.3f}, '
           f'test acc {test_acc:.3f}')
-    print(f'{metric[2] * num_epochs / timer.sum():.1f} examples/sec '
-          f'on {str(device)}')
