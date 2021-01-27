58875ac2722942aea07f2c7b8bf64712235660c8
Jayy <47641025+Jayanth49@users.noreply.github.com>
2021-01-27 19:29:45 +0530

    added decimate,auxillary layers

--- Deep_Learning/vision_concepts-and-Projects/SSD/SSD/model.py
+++ Deep_Learning/vision_concepts-and-Projects/SSD/SSD/model.py
@@ -7,7 +7,7 @@ Created on Wed Jan 27 16:01:59 2021
 
 import torch
 from torch import nn
-from utils import *
+from utils import decimate
 
 import torch.nn.functional as F
 from math import sqrt
@@ -140,9 +140,93 @@ class VGGBase(nn.Module):
         print("\nLoaded base model.\n")
         
         
-
-            
+class AuxiliaryConvolutions(nn.Module):
+    """
+    Additional convolutions to produce higher-level feature maps.
+    """
+    
+    
+    def __init__(self):
+        super(AuxiliaryConvolutions, self).__init__()
         
+        # Auxiliary/additional convolutions on top of the VGG base
+        self.conv8_1 = nn.Conv2d(1024,256,kernel_size=1,padding=0) # stride 1 by default
+        self.conv8_2 = nn.Conv2d(256,512,kernel_size=3,stride=2,padding=1) # dim reduces as stride as > 1
+        
+        self.conv9_1 = nn.Conv2d(512,128,kernel_size=1,padding=0)
+        self.conv9_2 = nn.Conv2d(128,256,kernel_size=3,stride=2,padding=1) # similar as above
+        
+        self.conv10_1 = nn.Conv2d(256,128,kernel_size=1,padding=0)
+        self.conv10_2 = nn.Conv2d(128,256,kernel_size=3,padding=0) # dims reduces as padding is  zero
+        
+        self.conv11_1 = nn.Conv2d(256,128,kernel_size=1,padding=0)
+        self.conv11_2 = nn.Conv2d(128,256,kernel_size=3,padding=0) # similar as above 
+        
+        # Initilising parameters
+        self.init_conv2d()
+        
+    def init_conv2d(self):
+        """
+        Initialize convolution parameters.
+        """
+        
+        for c in  self.children():
+            if isinstance(c,nn.Conv2d):
+                nn.init.xavier_uniform(c.weight)
+                nn.init.constant_(c.bias,0.)
+                
+    def forward(self,conv7_feats):
+        """
+        Forward propagation.
+        :param conv7_feats: lower-level conv7 feature map, a tensor of dimensions (N, 1024, 19, 19)
+        :return: higher-level feature maps conv8_2, conv9_2, conv10_2, and conv11_2
+        """     
+        
+        out = F.relu(self.conv8_1(conv7_feats)) # (N, 256, 19, 19)
+        out = F.relu(self.conv8_2(out)) # (N, 512, 10, 10)
+        conv8_2_feats = out
+        
+        out = F.relu(self.conv9_1(out)) # (N, 128, 10, 10)
+        out = F.relu(self.conv9_2(out)) # (N, 256, 5, 5)
+        conv9_2_feats = out
+        
+        out = F.relu(self.conv10_1(out)) # (N, 128, 5, 5)
+        out = F.relu(self.conv10_2(out)) # (N, 256, 3, 3)
+        conv10_2_feats = out
+        
+        out = F.relu(self.conv11_1(out)) # (N, 128, 3, 3)
+        out = F.relu(self.conv11_2(out)) # (N, 256, 1, 1)
+        conv11_2_feats = out
+        
+        # Higher level features
+        return conv8_2_feats,conv9_2_feats,conv10_2_feats,conv11_2_feats
+    
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+    
+            
         
         
         
